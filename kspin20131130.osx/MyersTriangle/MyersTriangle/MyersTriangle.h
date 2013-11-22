213a98dd8e26f521f11476639673f9d2ca6bae08
Osamu Kobayashi <osamu-k@arion.ocn.ne.jp>
2013-11-22 22:08:21 +0900

    First commit of MyersTriangle for Mac OSX.

---
+++ kspin20131130.osx/MyersTriangle/MyersTriangle/MyersTriangle.h
@@ -0,0 +1,15 @@
+#ifndef MYERS_TRIANGLE_H
+#define MYERS_TRIANGLE_H
+
+typedef enum
+{
+	ScaleneTriangle   = 1,	// 不等辺三角形
+	RegularTriangle   = 2,	// 正三角形
+	IsoscelesTriangle = 3,	// 二等辺三角形
+	NotTriangle       = 4	// 三角形ではない
+} TriangleType;
+
+TriangleType VerifyTriangle(int a, int b, int c);
+
+#endif // MYERS_TRIANGLE_H
+
