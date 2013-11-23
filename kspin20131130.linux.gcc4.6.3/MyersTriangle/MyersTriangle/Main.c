5e4e064cdc22398f1993683ee3921b21a399942c
Osamu Kobayashi <osamu@osamu-VirtualBox.(none)>
2013-11-23 16:32:32 +0900

    First commit of MyersTriangle for linux gcc4.6.3

---
+++ kspin20131130.linux.gcc4.6.3/MyersTriangle/MyersTriangle/Main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "MyersTriangle.h"
+
+int main(int argc, char** argv)
+{
+	int a, b, c;
+
+	while(1)
+	{
+		printf("3辺の値を入力してください¥n");
+	
+		printf("a辺の長さ： ");
+		scanf("%d", &a);
+		printf("b辺の長さ： ");
+		scanf("%d", &b);
+		printf("c辺の長さ： ");
+		scanf("%d", &c);
+	
+		switch(VerifyTriangle(a, b, c))
+		{
+		case RegularTriangle:
+			printf("正三角形¥n");
+			break;
+		case IsoscelesTriangle:
+			printf("二等辺三角形¥n");
+			break;
+		case ScaleneTriangle:
+			printf("不等辺三角形¥n");
+			break;
+		case NotTriangle:
+			printf("三角形ではありません¥n");
+			break;
+		default:
+			printf("入力が不正です¥n");
+			break;
+		}
+		printf("¥n");
+	}
+
+	return 0;
+}
---
+++ kspin20131130.linux.gcc4.6.3/MyersTriangle/Test/MyersTriangleTest.cpp
@@ -0,0 +1,24 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+#include "MyersTriangle.h"
+}
+
+TEST_GROUP(MyersTriangle)
+{
+	void setup()
+	{
+	
+	}
+
+	void teardown()
+	{
+
+	}
+};
+
+IGNORE_TEST(MyersTriangle, FirstTest)
+{
+	CHECK_EQUAL(1, 2);
+}
